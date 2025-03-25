df2a51d69c7390db8a1521d888287e286d440c59
hieunm2000 <data.minhhieu@gmail.com>
2025-03-25 13:07:44 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/clockecommerce/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.clockecommerce
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
+}
